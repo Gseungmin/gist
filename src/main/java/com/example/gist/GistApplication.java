a576a19f18acdb721b754051dde0e51e4ae27d6e
Gseungmin <cswcsm02@gmail.com>
2025-06-14 00:41:07 +0900

    init

---
+++ src/main/java/com/example/gist/GistApplication.java
@@ -0,0 +1,13 @@
+package com.example.gist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gist/GistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
